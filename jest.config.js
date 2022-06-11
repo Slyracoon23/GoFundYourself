c9ba7dbc3635389f0850a59241e3ec54e999fc89
slyRacoon23 <earl.potters@gmail.com>
2022-06-11 13:17:37 -0600

    semi working jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+    "\\.js$": "<rootDir>/node_modules/babel-jest"
+
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/fileMock.js",
+  },
+}
---
+++ test/pages/index.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render, screen, fireEvent } from '../testUtils'
+import Home from '../../pages/index'
+
+describe('Home page', () => {
+  // it('matches snapshot', () => {
+  //   const { asFragment } = render(<Home />, {})
+  //   expect(asFragment()).toMatchSnapshot()
+  // })
+
+  // it('clicking button triggers alert', () => {
+  //   const { getByText } = render(<Home />, {})
+
+  //   window.alert = jest.fn()
+  //   fireEvent.click(getByText('Test Button'))
+  //   expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
+  // })
+
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', {
+      name: /welcome to next\.js!/i,
+    })
+
+    // expect(heading).toBeInTheDocument()
+  })
+})
+})
